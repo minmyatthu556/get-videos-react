309815feda70c040e8cc7ffccd26c1e37ab267c9
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-04-25 22:48:56 +0630

    set up search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className={"ui container"}>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className={"search-bar ui segment"} style={{ marginTop: "10px" }}>
+        <form className={"ui form"} onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="search">Video Search</label>
+            <input
+              type="text"
+              id={"search"}
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
