9208dd57e840e42898dd75b6678cababd34bf84b
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-04-25 23:29:07 +0630

    finish setting up youtube api to fetch five videos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import SearchBar from "./components/SearchBar";
 import youtube from "./api/youtube";
 
 class App extends Component {
-  onTermSubmit = (term) => {
-    console.log(term);
-  }
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search/", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
 
   render() {
     return (
       <div className={"ui container"}>
-        <SearchBar onSubmit={this.onTermSubmit}/>
+        <SearchBar onSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} video
       </div>
     );
   }
